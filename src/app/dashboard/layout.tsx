a07306ffbafb462a9a1e5a54e6529ad3e01d1452
Gianluca Bredice <bredicegian@gmail.com>
2024-11-07 16:50:59 -0300

    fix: typescript error and revert background color of header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function AdminLayout({ children }: TProps) {
   return (
     <>
       <Header
-        className="p-8 z-50 flex items-center w-full justify-between sticky top-0 bg-secondary/50 backdrop-blur-sm"
+        className="p-8 z-50 flex items-center w-full justify-between sticky top-0 bg-white"
         logoSize="h-6"
         isForAdmin
       />
